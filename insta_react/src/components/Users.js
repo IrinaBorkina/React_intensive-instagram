0b745ce9f3e25d371b66f9245fbf74860303560c
Irina Borkina <irina_4ever@mail.ru>
2019-05-27 16:05:58 +0300

    feat: add right field

---
+++ insta_react/src/components/Users.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import User from './User';
+
+export default function Users() {
+    return (
+        <div className="right">
+            <User 
+                src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
+                alt="girl"
+                name="Iren"
+            />
+            <div className="users__block">
+                <User 
+                    src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
+                    alt="girl"
+                    name="Iren"
+                    min
+                />
+                <User 
+                    src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
+                    alt="girl"
+                    name="Iren"
+                    min
+                />
+                <User 
+                    src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
+                    alt="girl"
+                    name="Iren"
+                    min
+                />
+                <User 
+                    src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
+                    alt="girl"
+                    name="Iren"
+                    min
+                />
+            </div>
+        </div>
+    )
+}
