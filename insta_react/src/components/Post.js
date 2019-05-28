3257a161178d00d68fe19941ab1428aba3d6f9fe
Irina Borkina <irina_4ever@mail.ru>
2019-05-28 09:47:29 +0300

    feat: add api

--- insta_react/src/components/Post.js
+++ insta_react/src/components/Post.js
@@ -9,6 +9,7 @@ export default class Post extends Component {
                     src="https://cdn.shopify.com/s/files/1/1299/7671/products/hummingbirds-blindfolded-art-print_large.png?v=1522294174"
                     alt="girl"
                     name="Iren"
+                    min
                 />
                 <img src={this.props.src} alt={this.props.alt}></img>
                 <div className="post__name">
